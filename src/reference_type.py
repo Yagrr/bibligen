08c37b7686f0fd5bf1414a0fdd84087039fc31be
Yagrr <90156764+Yagrr@users.noreply.github.com>
2026-05-18 13:46:27 +0100

    refactor(refgen): separate ReferenceType from ReferenceModel

---
+++ src/reference_type.py
@@ -0,0 +1,270 @@
+from enum import Enum
+
+class ReferenceType(Enum):
+    """
+    Enum class to serve as document templates.
+    Document types and fields based on `https://bibtex.eu/types/` and
+    `https://www.bibtex.com/e/entry-types/`
+
+    TODO: For MISC item type, allow users to add custom fields.
+    """
+    ARTICLE = {
+        "key": "",
+        "item_type": "@article",
+        "author": [],
+        "title": "",
+        "journal": "",
+        "year": "",
+
+        "publisher": "",
+        "volume": "",
+        "number": "",
+        "pages": "",
+        "issn": "",
+        "doi": "",
+        "url": "",
+        "note": "",
+    }
+    BOOK = {
+        "key": "",
+        "item_type": "@book",
+        "author": [],
+        "title": "",
+        "publisher": "",
+        "address": "",
+        "year": "",
+
+        "editor": [],
+        "series": "",
+        "volume": "",
+        "number": "",
+        "edition": "",
+        "note": "",
+        # Non standard
+        "doi": "",
+        "issn": "",
+        "isbn": "",
+        "url": "",
+    }
+    BOOKLET = {
+        "key": "",
+        "item_type": "@book",
+        "author": [],
+        "title": "",
+        "howpublished": "",
+        "address": "",
+        "year": "",
+
+        "editor": [],
+        "series": "",
+        "volume": "",
+        "number": "",
+        "organization": "",
+        "monthy": "",
+        "note": "",
+
+        "url": "",
+    }
+    CONFERENCE = {
+        "key": "",
+        "item_type": "@conference",
+        "author": [],
+        "title": "",
+        "booktitle": "",
+        "year": "",
+
+        "editor": "",
+        "series": "",
+        "volume": "",
+        "number": "",
+        "pages": "",
+        "address": "",
+        "month": "",
+        "organization": "",
+        "publisher": "",
+        "note": "",
+
+        "url": "",
+    }
+    INBOOK = {
+        "key": "",
+        "item_type": "@inbook",
+        "author": [],
+        "title": "",
+        "booktitle": "",
+        "publisher": "",
+        "year": "",
+
+        "editor": [],
+        "series": "",
+        "volume": "",
+        "number": "",
+        "address": "",
+        "edition": "",
+        "month": "",
+        "pages": "",
+        "note": "",
+
+        "url": "",
+    }
+    INCOLLECTION = {
+        "key": "",
+        "item_type": "@incollection",
+        "author": [],
+        "title": "",
+        "booktitle": "",
+        "publisher": "",
+        "year": "",
+
+        "editor": [],
+        "series": "",
+        "volume": "",
+        "number": "",
+        "edition": "",
+        "address": "",
+        "month": "",
+        "pages": "",
+        "note": "",
+
+        "url": "",
+    }
+    INPROCEEDINGS = {
+        "key": "",
+        "item_type": "@inproceedings",
+        "author": [],
+        "title": "",
+        "booktitle": "",
+        "year": "",
+
+        "editor": [],
+        "series": "",
+        "volume": "",
+        "number": "",
+        "address": "",
+        "month": "",
+        "organization": "",
+        "publisher": "",
+        "note": "",
+
+        "url": "",
+    }
+    MANUAL = {
+        "key": "",
+        "item_type": "@manual",
+        "author": [],
+        "title": "",
+        "booktitle": "",
+        "year": "",
+
+        "volume": "",
+        "number": "",
+        "edition": "",
+        "address": "",
+        "month": "",
+        "organization": "",
+        "note": "",
+
+        "url": "",
+    }
+    MASTERSTHESIS = {
+        "key": "",
+        "item_type": "@mastersthesis",
+        "author": [],
+        "title": "",
+        "school": "",
+        "year": "",
+
+        "type": "",
+        "address": "",
+        "month": "",
+        "note": "",
+
+        "url": "",
+    }
+    """ ALLOW USERS TO ADD CUSTOM FIELDS FOR MISC TYPE """
+    MISC = {
+        "key": "",
+        "item_type": "@misc",
+        "author": [],
+        "title": "",
+        "year": "",
+        "howpublished": "",
+        "note": "",
+        "url": "",
+    }
+    PHDTHESIS = {
+        "key": "",
+        "item_type": "@mastersthesis",
+        "author": [],
+        "title": "",
+        "school": "",
+        "year": "",
+
+        "type": "",
+        "address": "",
+        "month": "",
+        "note": "",
+
+        "url": "",
+    }
+    PROCEEDINGS = {
+        "key": "",
+        "item_type": "@proceedings",
+        "editor": [],
+        "title": "",
+        "school": "",
+        "year": "",
+
+        "series": "",
+        "volume": "",
+        "number": "",
+        "address": "",
+        "month": "",
+        "publisher": "",
+        "note": "",
+
+        "url": "",
+    }
+    TECHREPORT = {
+        "key": "",
+        "item_type": "@techreport",
+        "author": [],
+        "title": "",
+        "institution": "",
+        "year": "",
+
+        "number": "",
+        "address": "",
+        "month": "",
+        "note": "",
+
+        "url": ""
+    }
+    # REPORT is non standard but included anyways because app use-case mainly applies
+    # to this document type
+    REPORT = {
+        "key": "",
+        "item_type": "@report",
+        "author": [],
+        "title": "",
+        "institution": "",
+        "year": "",
+
+        "number": "",
+        "address": "",
+        "month": "",
+        "note": "",
+
+        "url": ""
+    }
+    UNPUBLISHED = {
+        "key": "",
+        "item_type": "@unpublished",
+        "author": [],
+        "title": "",
+        "year": "",
+
+        "note": "",
+
+        "url": ""
+    }
